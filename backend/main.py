6de3661e3fd361d8bb497fa642694bb315e46ed6
ToD <davenportcaster@yahoo.com>
2024-10-19 17:25:57 +0800

    test model

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify, request
-from services.utils import allowed_file, recognize_image
+from services.utils import allowed_file, recognize_image, model_details
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['upload_folder'] = './uploaded_images'
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({"message": "API server is running"})
 
 @app.route('/api/hello', methods=['GET'])
 def hello_world():
@@ -12,6 +16,11 @@ def hello_world():
 @app.route('/api/upload_image', methods=['GET'])
 def get_upload_image():
     return jsonify({"message": "Upload Image"})
+
+@app.route('/api/model_details', methods=['GET'])
+def get_model_details():
+    model_details()
+    return jsonify({"message": "Model Details"})
 
 @app.route('/api/upload_image', methods=['POST'])
 def post_upload_image():
--- backend/services/utils.py
+++ backend/services/utils.py
@@ -1,4 +1,8 @@
+import onnx
+import onnxruntime as rt
+
 ALLOWERD_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+MODEL = onnx.load('model/best.onnx')
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWERD_EXTENSIONS
@@ -7,3 +11,7 @@ def recognize_image(image_path):
     food_name = 'food_name'
     calories = '100kcal per 100g'
     return food_name, calories
+
+def model_details():
+    print(MODEL)
+    return ""
