4a8aa48d595b77a9cb13d2966e5d1867cd534fbd
wangxiang-yolo <e1352903@u.nus.edu>
2024-10-22 11:53:11 +0800

    Update ResultsPage.js

--- src/ResultsPage.js
+++ src/ResultsPage.js
@@ -1,32 +1,30 @@
-// src/components/ResultsPage.js
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import './ResultsPage.css';
 import Chart from 'chart.js/auto';
-import axios from 'axios'; // 引入 Axios
+import axios from 'axios'; 
 
 const ResultsPage = () => {
   const { state } = useLocation();
   const { foodType, calories, nutritionInfo } = state;
 
-  // 假设 nutritionInfo 是一个对象，其中包含各个营养成分
-  const { carbohydrates, protein, fat } = nutritionInfo; // 从 nutritionInfo 中提取各个成分
+  const { carbohydrates, protein, fat } = nutritionInfo; 
 
   useEffect(() => {
     const ctx = document.getElementById('nutritionChart').getContext('2d');
     const nutritionData = {
-      labels: ['Carbohydrates', 'Protein', 'Fat'], // 根据 nutritional info 填充
+      labels: ['Carbohydrates', 'Protein', 'Fat'], 
       datasets: [{
         label: 'Nutritional Breakdown',
-        data: [carbohydrates, protein, fat], // 使用从 nutritionInfo 中提取的数据
+        data: [carbohydrates, protein, fat], 
         backgroundColor: ['#ff6384', '#36a2eb', '#ffce56'],
         hoverBackgroundColor: ['#ff6384', '#36a2eb', '#ffce56'],
       }],
     };
 
-    // 初始化饼图
+  
     new Chart(ctx, {
-      type: 'pie', // 使用饼图
+      type: 'pie', 
       data: nutritionData,
     });
   }, [nutritionInfo]);
@@ -49,8 +47,8 @@ const ResultsPage = () => {
       <h2>Food Recognition Results</h2>
       <h3>Food Item: {foodType}</h3>
       <p>Estimated Calories: {calories} kcal</p>
-      <p>Nutritional Information: {JSON.stringify(nutritionInfo)}</p> {/* 可以更好地格式化显示营养信息 */}
-      <canvas id="nutritionChart" width="200" height="200"></canvas> {/* 修改宽度和高度 */}
+      <p>Nutritional Information: {JSON.stringify(nutritionInfo)}</p> 
+      <canvas id="nutritionChart" width="200" height="200"></canvas> }
       <button onClick={getDietarySuggestions}>Get Dietary Suggestions</button>
     </div>
   );
