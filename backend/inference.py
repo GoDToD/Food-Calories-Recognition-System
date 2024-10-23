b554b1071dd929cdd48f6089814a3cac2fa7afff
ZYG-rr <zhuyinge18@gmail.com>
2024-10-23 15:50:16 +0800

    Add files via upload
    onnx infer

---
+++ backend/inference.py
@@ -0,0 +1,28 @@
+from ultralytics import YOLO
+from services.food_calories_data import food_calories, food_labels
+
+# # Load the YOLO11 model
+# model = YOLO("yolo11n.pt")
+
+# # Export the model to ONNX format
+# model.export(format="onnx")  # creates 'yolo11n.onnx'
+
+# Load the exported ONNX model
+onnx_model = YOLO(r"D:\projects\Food\runs\train\food101_experiment\weights\best.onnx", task="detect")
+
+# Run inference with the correct input size
+#results = onnx_model.predict("D:\projects\Food\data\images\\train\waffles_3852270.jpg", imgsz=(320, 320))
+results = onnx_model.predict(r'D:\projects\Food\data\images\train\chicken_wings_3282233.jpg', imgsz=(320, 320))
+for result in results:
+    # Extract the class IDs and map them to names
+    class_ids = result.boxes.cls.cpu().numpy().astype(int)  # Extract the class IDs
+    class_names = [result.names[class_id] for class_id in class_ids]  # Map class IDs to names
+
+    # Print the detected class names
+    for class_name in class_names:
+        
+        calories = food_calories[class_name]
+        print("Detected classes:", class_name)
+        print("Detected calories", calories)
+        
+      
